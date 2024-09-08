4aed250be953ad28c76a15c500d5bea812056df2
saba-ramezani <saba.rad.sr@gmail.com>
2024-09-08 12:39:44 +0330

    added navbar for large devices

---
+++ components/Button.tsx
@@ -0,0 +1,23 @@
+import Image from 'next/image';
+import React from 'react'
+
+type ButtonProps = {
+    type: "button" | "submit";
+    title: string;
+    icon?: string;
+    variant: "btn_dark_green"
+}
+
+const Button = ({type, title, icon, variant}: ButtonProps) => {
+  return (
+    <button
+        type={type}
+        className={`flexCenter gap-3 rounded-full border ${variant}`}
+    >
+        {icon && <Image src={icon} alt={title} width={24} height={24}/>}
+        <label className='bold-16 whitespace-nowrap'>{title}</label>
+    </button>
+  )
+}
+
+export default Button
